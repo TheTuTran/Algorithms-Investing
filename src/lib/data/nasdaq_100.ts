0bbf3652f34aa309730b0ec43d6bb23896b23486
TheTuTran <131922588+TheTuTran@users.noreply.github.com>
2024-08-30 18:37:01 -0500

    fixed nasdaq_100 to include sector

--- src/lib/data/nasdaq_100.ts
+++ src/lib/data/nasdaq_100.ts
@@ -2,405 +2,506 @@ export const snp_array = [
   {
     Symbol: "AAPL ",
     Name: "Apple Inc",
+    "GICS Sector": "Information Technology",
   },
   {
     Symbol: "NVDA ",
     Name: "NVIDIA Corp",
+    "GICS Sector": "Information Technology",
   },
   {
     Symbol: "MSFT ",
     Name: "Microsoft Corp",
+    "GICS Sector": "Information Technology",
   },
   {
     Symbol: "AVGO ",
     Name: "Broadcom Inc",
+    "GICS Sector": "Information Technology",
   },
   {
     Symbol: "META ",
     Name: "Meta Platforms Inc",
+    "GICS Sector": "Communication Services",
   },
   {
     Symbol: "AMZN ",
     Name: "Amazon.com Inc",
+    "GICS Sector": "Consumer Discretionary",
   },
   {
     Symbol: "COST ",
     Name: "Costco Wholesale Corp",
+    "GICS Sector": "Consumer Staples",
   },
   {
     Symbol: "TSLA ",
     Name: "Tesla Inc",
+    "GICS Sector": "Consumer Discretionary",
   },
   {
     Symbol: "GOOGL ",
     Name: "Alphabet Inc",
+    "GICS Sector": "Communication Services",
   },
   {
     Symbol: "GOOG ",
     Name: "Alphabet Inc",
+    "GICS Sector": "Communication Services",
   },
   {
     Symbol: "NFLX ",
     Name: "Netflix Inc",
+    "GICS Sector": "Communication Services",
   },
   {
     Symbol: "ADBE ",
     Name: "Adobe Inc",
+    "GICS Sector": "Information Technology",
   },
   {
     Symbol: "AMD ",
     Name: "Advanced Micro Devices Inc",
+    "GICS Sector": "Information Technology",
   },
   {
     Symbol: "PEP ",
     Name: "PepsiCo Inc",
+    "GICS Sector": "Consumer Staples",
   },
   {
     Symbol: "TMUS ",
     Name: "T-Mobile US Inc",
+    "GICS Sector": "Communication Services",
   },
   {
     Symbol: "LIN ",
     Name: "Linde PLC",
+    "GICS Sector": "Materials",
   },
   {
     Symbol: "CSCO ",
     Name: "Cisco Systems Inc",
+    "GICS Sector": "Information Technology",
   },
   {
     Symbol: "QCOM ",
     Name: "QUALCOMM Inc",
+    "GICS Sector": "Information Technology",
   },
   {
     Symbol: "TXN ",
     Name: "Texas Instruments Inc",
+    "GICS Sector": "Information Technology",
   },
   {
     Symbol: "AMGN ",
     Name: "Amgen Inc",
+    "GICS Sector": "Health Care",
   },
   {
     Symbol: "INTU ",
     Name: "Intuit Inc",
+    "GICS Sector": "Information Technology",
   },
   {
     Symbol: "ISRG ",
     Name: "Intuitive Surgical Inc",
+    "GICS Sector": "Health Care",
   },
   {
     Symbol: "AMAT ",
     Name: "Applied Materials Inc",
+    "GICS Sector": "Information Technology",
   },
   {
     Symbol: "CMCSA ",
     Name: "Comcast Corp",
+    "GICS Sector": "Communication Services",
   },
   {
     Symbol: "HON ",
     Name: "Honeywell International Inc",
+    "GICS Sector": "Industrials",
   },
   {
     Symbol: "BKNG ",
     Name: "Booking Holdings Inc",
+    "GICS Sector": "Consumer Discretionary",
   },
   {
     Symbol: "REGN ",
     Name: "Regeneron Pharmaceuticals Inc",
+    "GICS Sector": "Health Care",
   },
   {
     Symbol: "VRTX ",
     Name: "Vertex Pharmaceuticals Inc",
+    "GICS Sector": "Health Care",
   },
   {
     Symbol: "ADI ",
     Name: "Analog Devices Inc",
+    "GICS Sector": "Information Technology",
   },
   {
     Symbol: "PANW ",
     Name: "Palo Alto Networks Inc",
+    "GICS Sector": "Information Technology",
   },
   {
     Symbol: "ADP ",
     Name: "Automatic Data Processing Inc",
+    "GICS Sector": "Industrials",
   },
   {
     Symbol: "SBUX ",
     Name: "Starbucks Corp",
+    "GICS Sector": "Consumer Discretionary",
   },
   {
     Symbol: "KLAC ",
     Name: "KLA Corp",
+    "GICS Sector": "Information Technology",
   },
   {
     Symbol: "MU ",
     Name: "Micron Technology Inc",
+    "GICS Sector": "Information Technology",
   },
   {
     Symbol: "LRCX ",
     Name: "Lam Research Corp",
+    "GICS Sector": "Information Technology",
   },
   {
     Symbol: "MELI ",
     Name: "MercadoLibre Inc",
+    "GICS Sector": "Consumer Discretionary",
   },
   {
     Symbol: "GILD ",
     Name: "Gilead Sciences Inc",
+    "GICS Sector": "Health Care",
   },
   {
     Symbol: "MDLZ ",
     Name: "Mondelez International Inc",
+    "GICS Sector": "Consumer Staples",
   },
   {
     Symbol: "INTC ",
     Name: "Intel Corp",
+    "GICS Sector": "Information Technology",
   },
   {
     Symbol: "CTAS ",
     Name: "Cintas Corp",
+    "GICS Sector": "Industrials",
   },
   {
     Symbol: "ASML ",
     Name: "ASML Holding NV",
+    "GICS Sector": "Information Technology",
   },
   {
     Symbol: "SNPS ",
     Name: "Synopsys Inc",
+    "GICS Sector": "Information Technology",
   },
   {
     Symbol: "PYPL ",
     Name: "PayPal Holdings Inc",
+    "GICS Sector": "Financials",
   },
   {
     Symbol: "CDNS ",
     Name: "Cadence Design Systems Inc",
+    "GICS Sector": "Information Technology",
   },
   {
     Symbol: "ORLY ",
     Name: "O'Reilly Automotive Inc",
+    "GICS Sector": "Consumer Discretionary",
   },
   {
     Symbol: "CSX ",
     Name: "CSX Corp",
+    "GICS Sector": "Industrials",
   },
   {
     Symbol: "MAR ",
     Name: "Marriott International Inc/MD",
+    "GICS Sector": "Consumer Discretionary",
   },
   {
     Symbol: "NXPI ",
     Name: "NXP Semiconductors NV",
+    "GICS Sector": "Information Technology",
   },
   {
     Symbol: "PDD ",
     Name: "PDD Holdings Inc ADR",
+    "GICS Sector": "Consumer Discretionary",
   },
   {
     Symbol: "CRWD ",
     Name: "Crowdstrike Holdings Inc",
+    "GICS Sector": "Information Technology",
   },
   {
     Symbol: "CEG ",
     Name: "Constellation Energy Corp",
+    "GICS Sector": "Utilities",
   },
   {
     Symbol: "MRVL ",
     Name: "Marvell Technology Inc",
+    "GICS Sector": "Information Technology",
   },
   {
     Symbol: "ROP ",
     Name: "Roper Technologies Inc",
+    "GICS Sector": "Information Technology",
   },
   {
     Symbol: "FTNT ",
     Name: "Fortinet Inc",
+    "GICS Sector": "Information Technology",
   },
   {
     Symbol: "WDAY ",
     Name: "Workday Inc",
+    "GICS Sector": "Information Technology",
   },
   {
     Symbol: "ADSK ",
     Name: "Autodesk Inc",
+    "GICS Sector": "Information Technology",
   },
   {
     Symbol: "CHTR ",
     Name: "Charter Communications Inc",
+    "GICS Sector": "Communication Services",
   },
   {
     Symbol: "AEP ",
     Name: "American Electric Power Co Inc",
+    "GICS Sector": "Utilities",
   },
   {
     Symbol: "ABNB ",
     Name: "Airbnb Inc",
+    "GICS Sector": "Consumer Discretionary",
   },
   {
     Symbol: "ROST ",
     Name: "Ross Stores Inc",
+    "GICS Sector": "Consumer Discretionary",
   },
   {
     Symbol: "AZN ",
     Name: "AstraZeneca PLC ADR",
+    "GICS Sector": "Health Care",
   },
   {
     Symbol: "CPRT ",
     Name: "Copart Inc",
+    "GICS Sector": "Industrials",
   },
   {
     Symbol: "MNST ",
     Name: "Monster Beverage Corp",
+    "GICS Sector": "Consumer Staples",
   },
   {
     Symbol: "PCAR ",
     Name: "PACCAR Inc",
+    "GICS Sector": "Industrials",
   },
   {
     Symbol: "KDP ",
     Name: "Keurig Dr Pepper Inc",
+    "GICS Sector": "Consumer Staples",
   },
   {
     Symbol: "DASH ",
     Name: "DoorDash Inc",
+    "GICS Sector": "Consumer Discretionary",
   },
   {
     Symbol: "PAYX ",
     Name: "Paychex Inc",
+    "GICS Sector": "Industrials",
   },
   {
     Symbol: "TTD ",
     Name: "Trade Desk Inc/The",
+    "GICS Sector": "Communication Services",
   },
   {
     Symbol: "MCHP ",
     Name: "Microchip Technology Inc",
+    "GICS Sector": "Information Technology",
   },
   {
     Symbol: "KHC ",
     Name: "Kraft Heinz Co/The",
+    "GICS Sector": "Consumer Staples",
   },
   {
     Symbol: "ODFL ",
     Name: "Old Dominion Freight Line Inc",
+    "GICS Sector": "Industrials",
   },
   {
     Symbol: "IDXX ",
     Name: "IDEXX Laboratories Inc",
+    "GICS Sector": "Health Care",
   },
   {
     Symbol: "EA ",
     Name: "Electronic Arts Inc",
+    "GICS Sector": "Communication Services",
   },
   {
     Symbol: "FAST ",
     Name: "Fastenal Co",
+    "GICS Sector": "Industrials",
   },
   {
     Symbol: "GEHC ",
     Name: "GE HealthCare Technologies Inc",
+    "GICS Sector": "Health Care",
   },
   {
     Symbol: "VRSK ",
     Name: "Verisk Analytics Inc",
+    "GICS Sector": "Industrials",
   },
   {
     Symbol: "CTSH ",
     Name: "Cognizant Technology Solutions Corp",
+    "GICS Sector": "Information Technology",
   },
   {
     Symbol: "EXC ",
     Name: "Exelon Corp",
+    "GICS Sector": "Utilities",
   },
   {
     Symbol: "CCEP ",
     Name: "Coca-Cola Europacific Partners PLC",
+    "GICS Sector": "Consumer Staples",
   },
   {
     Symbol: "DDOG ",
     Name: "Datadog Inc",
+    "GICS Sector": "Information Technology",
   },
   {
     Symbol: "BKR ",
     Name: "Baker Hughes Co",
+    "GICS Sector": "Energy",
   },
   {
     Symbol: "FANG ",
     Name: "Diamondback Energy Inc",
+    "GICS Sector": "Energy",
   },
   {
     Symbol: "XEL ",
     Name: "Xcel Energy Inc",
+    "GICS Sector": "Utilities",
   },
   {
     Symbol: "LULU ",
     Name: "Lululemon Athletica Inc",
+    "GICS Sector": "Consumer Discretionary",
   },
   {
     Symbol: "ON ",
     Name: "ON Semiconductor Corp",
+    "GICS Sector": "Information Technology",
   },
   {
     Symbol: "SMCI ",
     Name: "Super Micro Computer Inc",
+    "GICS Sector": "Information Technology",
   },
   {
     Symbol: "CSGP ",
     Name: "CoStar Group Inc",
+    "GICS Sector": "Real Estate",
   },
   {
     Symbol: "CDW ",
     Name: "CDW Corp/DE",
+    "GICS Sector": "Information Technology",
   },
   {
     Symbol: "MRNA ",
     Name: "Moderna Inc",
+    "GICS Sector": "Health Care",
   },
   {
     Symbol: "BIIB ",
     Name: "Biogen Inc",
+    "GICS Sector": "Health Care",
   },
   {
     Symbol: "ZS ",
     Name: "Zscaler Inc",
+    "GICS Sector": "Information Technology",
   },
   {
     Symbol: "DXCM ",
     Name: "Dexcom Inc",
+    "GICS Sector": "Health Care",
   },
   {
     Symbol: "ANSS ",
     Name: "ANSYS Inc",
+    "GICS Sector": "Information Technology",
   },
   {
     Symbol: "TTWO ",
     Name: "Take-Two Interactive Software Inc",
+    "GICS Sector": "Communication Services",
   },
   {
     Symbol: "TEAM ",
     Name: "Atlassian Corp",
+    "GICS Sector": "Information Technology",
   },
   {
     Symbol: "GFS ",
     Name: "GLOBALFOUNDRIES Inc",
+    "GICS Sector": "Information Technology",
   },
   {
     Symbol: "ILMN ",
     Name: "Illumina Inc",
+    "GICS Sector": "Health Care",
   },
   {
     Symbol: "DLTR ",
     Name: "Dollar Tree Inc",
+    "GICS Sector": "Consumer Staples",
   },
   {
     Symbol: "WBD ",
     Name: "Warner Bros Discovery Inc",
+    "GICS Sector": "Communication Services",
   },
   {
     Symbol: "MDB ",
     Name: "MongoDB Inc",
+    "GICS Sector": "Information Technology",
   },
   {
     Symbol: "ARM ",
     Name: "ARM Holdings PLC ADR",
+    "GICS Sector": "Information Technology",
   },
 ];
